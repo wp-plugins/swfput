7a743412618ac466ac5520135bf594be709be31f
EdHynan <EdHynan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-05 18:38:21 +0000

    3.0.3, correcting version in header (sigh).
    git-svn-id: https://plugins.svn.wordpress.org/swfput/trunk@1238880 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- swfput.php
+++ swfput.php
@@ -3,7 +3,7 @@
 Plugin Name: SWFPut
 Plugin URI: http://agalena.nfshost.com/b1/swfput-html5-flash-wordpress-plugin
 Description: Add Flash and HTML5 video to WordPress posts, pages, and widgets, from arbitrary URI's or media library ID's or files in your media upload directory tree (including uploads not in the WordPress media library).
-Version: 3.0.2
+Version: 3.0.3
 Author: Ed Hynan
 Author URI: http://agalena.nfshost.com/b1/
 License: GNU GPLv3 (see http://www.gnu.org/licenses/gpl-3.0.html)
